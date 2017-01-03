108a3882b257f3b93a6a0be2a59218a947222ca9
Romanna Koval <rkoval@evolutiongaming.com>
2017-01-03 17:00:23 +0200

    make strategies as case-objects

--- src/main/scala/com/svitovyda/configuration/ConfigurationsController.scala
+++ src/main/scala/com/svitovyda/configuration/ConfigurationsController.scala
@@ -11,7 +11,40 @@ object Info {
   implicit val writes: Writes[Info] = Json.writes[Info]
 }
 
-case class Business(strategies: Set[String], someBoolean: Boolean)
+sealed trait Strategy {
+  def description: String
+  def name: String = toString
+}
+object Strategy {
+  val values: Set[Strategy] = Set(
+    Random, Incremental, Postponed, AlwaysWin, AlwaysLoose, Exponential, Gaussian
+  )
+
+  def apply(name: String): Strategy = values find (_.name.toLowerCase == name.toLowerCase) getOrElse {
+    sys.error(s"Cannot find the strategy named: $name")
+  }
+
+  case object Random extends Strategy {def description = "Simply random"}
+  case object Incremental extends Strategy {def description = "Simply one by one"}
+  case object Postponed extends Strategy {def description = "Do only once and stop"}
+  case object AlwaysWin extends Strategy {def description = "One side always wins"}
+  case object AlwaysLoose extends Strategy {def description = "One side always loose"}
+  case object Exponential extends Strategy {def description = "Exponential distribution"}
+  case object Gaussian extends Strategy {def description = "Gaussian distribution"}
+
+  implicit val format: Format[Strategy] = Format[Strategy](
+    Reads[Strategy] { js =>
+      values find {
+        _.name.toLowerCase == js.as[String].toLowerCase
+      } map (JsSuccess(_)) getOrElse JsError(s"Could not read Strategy: $js")
+    },
+    Writes[Strategy] { s =>
+      JsObject(Seq("name" -> Json.toJson(s.name), "description" -> Json.toJson(s.description)))
+    }
+  )
+}
+
+case class Business(strategies: Set[Strategy], someBoolean: Boolean)
 object Business {
   implicit val writes: Writes[Business] = Json.writes[Business]
 }
--- src/main/scala/com/svitovyda/configuration/Settings.scala
+++ src/main/scala/com/svitovyda/configuration/Settings.scala
@@ -6,10 +6,11 @@ import scala.collection.JavaConversions._
 
 
 case class Settings(config: Config) {
-  val strategies: Set[String] = config.getStringList("play254.settings.strategies").toSet
+  val strategies: Set[Strategy] = config.getStringList("play254.settings.strategies").toSet.map {
+    s: String => Strategy(s)
+  }
   val anyBool: Boolean = config.getBoolean("play254.settings.any-bool")
 }
-
 
 object Settings {
   implicit def conf2Settings: (Config) => Settings = Settings apply
